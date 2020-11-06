b7944e5fd8811f3a9b0d1f54b0cb38a1cc9a1c8f
ddemidov <ddemidov@yandex-team.ru>
2020-11-07 02:01:42 +0300

    Add echobot.py

---
+++ code/echobot.py
@@ -0,0 +1,12 @@
+import telebot
+
+token = ''
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
