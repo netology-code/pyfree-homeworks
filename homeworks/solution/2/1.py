e088652ac512121e1110d4703315266865cdc209
eshmargunov <eshmargunov@gmail.com>
2025-04-12 18:31:20 +0300

    update second hw

--- homeworks/solution/2/1.py
+++ homeworks/solution/2/1.py
@@ -1,31 +1,25 @@
-todos = []
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
 
+tasks = []
 
-HELP = '''
-Список доступных команд:
-* print/show  - напечать все задачи на заданную дату
-* todo - добавить задачу
-* help - Напечатать help
-'''
+run = True
 
-stop = False 
-while not stop: 
-    command = input('Введите команду: ')
-    if command == 'help':
-        print(HELP)
-    elif command == 'todo':
-        task = input('Введите задачу: ')
-        todos.append(task)
-        # Про синтаксис f-строк можно прочитать, например, тут:
-        # https://pythonru.com/osnovy/formatirovanie-v-python-s-pomoshhju-f-strok
-        # Альтернативный вариант:
-        # print('Задача ' + task + ' добавлена')
-        print(f'Задача {task} добавлена')
-    elif command == 'print' or command == 'show':
-        print(todos)
-    elif command == 'exit':
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(tasks)
+  elif command == "add":
+    task = input("Введите название задачи: ")
+    tasks.append(task)
+    print("Задача добавлена")
+  elif command == 'exit':
       print('Спасибо за использование! До свидания!')
-      stop = True
-    else:
-      print('Неизвестная команда!')
-      print(HELP)
+      run = False
+  else: 
+    print("Неизвестная команда")
+    break
--- homeworks/solution/2/2.py
+++ homeworks/solution/2/2.py
@@ -2,39 +2,37 @@ today = list() # today = [] # список можно создать двумя 
 tomorrow = list() # tomorrow = []
 other = list() # other = []
 
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
 
-HELP = '''
-Список доступных команд:
-* show - напечать все задачи
-* todo - добавить задачу
-* help - Напечатать help
-'''
+run = True
 
-
-while True:
-    command = input('Введите команду\n')
-    if command == 'help':
-        print(HELP)
-    elif command == 'todo':
-        date = input('Введите дату: ')
-        task = input('Введите задачу: ')
-        if date == 'Сегодня': 
-          today.append(task)
-        elif date == 'Завтра':
-          tomorrow.append(task)
-        else:
-          other.append(task)
-        print(f'Задача {task} добавлена')
-    elif command == 'show':
-        print('Сегодня')
-        print(today)      
-        print('Завтра')
-        print(tomorrow)
-        print('Другие')
-        print(other)
-    elif command == 'exit':
-        print('Спасибо за использование! До свидания!')
-        break
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == 'show':
+    print('Сегодня')
+    print(today)
+    print('Завтра')
+    print(tomorrow)
+    print('Другие')
+    print(other)
+  elif command == 'add':
+    date = input('Введите дату: ')
+    task = input('Введите задачу: ')
+    if date == 'Сегодня':
+      today.append(task)
+    elif date == 'Завтра':
+      tomorrow.append(task)
     else:
-        print('Неизвестная команда!')
-        print(HELP)
+      other.append(task)
+    print(f'Задача {task} добавлена')
+  elif command == 'exit':
+      print('Спасибо за использование! До свидания!')
+      run = False
+  else:
+    print("Неизвестная команда")
+    break
