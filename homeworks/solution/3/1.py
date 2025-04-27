b7f2b9b9a1756314f42839d87163e44879e271c3
eshmargunov <eshmargunov@gmail.com>
2025-04-27 12:24:45 +0300

    update third hw

--- homeworks/solution/3/1.py
+++ homeworks/solution/3/1.py
@@ -1,15 +1,15 @@
 def counter_letter(worlds, letter):
-    """Считает косличество слов с списке по заданному символу"""
+    """Считает количество слов в списке по заданному символу"""
 
-    count = 0  # созаём счётчик
+    count = 0  # создаём счётчик
     for world in worlds:  # проходимся по каждому слову world в списке слов worlds 
         if letter.lower() in world.lower():  # приводим к нижнему регистру
-            count += 1  # это тоже самое что и count = count + 1
+            count += 1  # это то же самое что и count = count + 1
     return count  # возвращаем результат
 
 
-worlds = ['python', 'c++', 'c', 'scala', 'java']
-letter = 'c'
+worlds = ['python', 'c++', 'c', 'scala', 'java'] # пример списка
+letter = 'c' # букву, которую ищем
 
-result = counter_letter(worlds, letter)
-print(result)
+result = counter_letter(worlds, letter) # вызываем функцию
+print(result) # выводим результат
